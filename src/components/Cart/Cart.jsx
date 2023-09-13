0183c52474b78b5bd75ead63ac2e47e3b7cd65b1
torikulswe <torikulswe@gmail.com>
2023-09-13 13:13:21 +0600

    showed data to Cart component as props

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 
-const Cart = () => {
+const Cart = ({ selectedActors }) => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="bg-gray-400 p-4 rounded-lg">
+      <h1 className="text-2xl text-white mb-4">
+        Selected Actors:{selectedActors.length}
+      </h1>
+      <div className="grid gap-4">
+        {selectedActors.map((actor) => (
+          <div
+            key={actor.id}
+            className="bg-gray-800 p-4 rounded-md shadow-lg flex items-center justify-between"
+          >
+            <div className="flex items-center">
+              <img
+                className="w-16 h-16 rounded-full object-cover mr-4"
+                src={actor.image}
+                alt=""
+              />
+              <h2 className="text-white text-lg">{actor.name}</h2>
+            </div>
+            <button className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-700">
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
+import Cart from "../Cart/Cart";
 
 const Home = () => {
   const [allActors, setAllActors] = useState([]);
+  const [selectedActors, setSelectedActors] = useState([]);
+
   useEffect(() => {
     fetch("./data.json")
       .then((res) => res.json())
       .then((data) => setAllActors(data));
   }, []);
-  console.log(allActors);
 
+  const handleSelectActor = (actor) => {
+    setSelectedActors([...selectedActors, actor]);
+  };
+  console.log(selectedActors);
   return (
     <div>
       <div className="home-container flex">
         <div className="w-3/4 flex flex-wrap gap-10">
           {allActors.map((actor) => (
-            <div className="w-96 bg-red-100 rounded-lg shadow-lg p-6">
+            <div
+              key={actor.id}
+              className="w-96 bg-red-100 rounded-lg shadow-lg p-6"
+            >
               <div className="flex justify-center mb-4">
                 <img
                   className="w-24 h-24 rounded-full"
@@ -24,27 +33,30 @@ const Home = () => {
                 />
               </div>
               <h2 className="text-2xl font-bold text-center text-orange-400 mb-2">
-              {actor.name}
+                {actor.name}
               </h2>
               <p className="text-xl text-center mb-4">
-                <small>
-                 {actor.description}
-                </small>
+                <small>{actor.description}</small>
               </p>
               <div className="flex justify-between mb-4">
                 <p className="text-lg font-bold text-green-600 mr-2">
                   Salary: {actor.salary} $
                 </p>
-                <p className="text-lg text-gray-600 font-semibold">{actor.role}</p>
+                <p className="text-lg text-gray-600 font-semibold">
+                  {actor.role}
+                </p>
               </div>
-              <button className="mx-auto bg-green-500 text-white px-6 py-2 rounded-lg">
+              <button
+                onClick={() => handleSelectActor(actor)}
+                className="mx-auto bg-green-500 text-white px-6 py-2 rounded-lg"
+              >
                 Select
               </button>
             </div>
           ))}
         </div>
         <div className="w-1/4">
-          <h2>This is cartt</h2>
+          <Cart selectedActors={selectedActors}></Cart>
         </div>
       </div>
     </div>
